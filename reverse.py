c014d3c249b05f89a812de4c0d0d1e51eb90d7d7
Raymond Chee <rjchee@gmail.com>
2016-04-15 05:07:56 -0500

    passes tests

--- reverse.py
+++ reverse.py
@@ -1,5 +1,55 @@
 import argparse
+import itertools
 import sys
+
+def split(text):
+    words = []
+    spaces = []
+    lastword = ''
+    lastspace = ''
+    for ch in text:
+        if ch.isspace():
+            if lastword:
+                words.append(lastword)
+                lastword = ''
+            lastspace += ch
+        else:
+            if lastspace:
+                spaces.append(lastspace)
+                lastspace = ''
+            lastword += ch
+    if lastword:
+        words.append(lastword)
+    elif lastspace:
+        spaces.append(lastspace)
+    return words, spaces
+
+
+def merge(words, spaces, wordFirst):
+    if wordFirst:
+        return ''.join(tk for pair in itertools.zip_longest(words, spaces, fillvalue='') for tk in pair)
+    return merge(spaces, words, True)
+
+def reverse(text, character, character_level, word, word_level):
+    if not text:
+        return text
+    if word:
+        words, spaces = split(text)
+        if word_level == 1 and not character:
+            words = words[::-1]
+        else:
+            words = sum((words[x:x+word_level][::-1] for x in range(0, len(words), word_level)), [])
+        if character:
+            for i in range(len(words)):
+                words[i] = reverse(words[i], character, character_level, False, 1)
+        res = merge(words, spaces, not text[0].isspace())
+    elif character:
+        if character_level == 1:
+            res = text[::-1]
+        else:
+            res = ''.join(text[x:x+character_level][::-1] for x in range(0, len(text), character_level))
+    return res
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Reverse the input")
@@ -26,10 +76,17 @@ if __name__ == '__main__':
             raise argparse.ArgumentTypeError("level must be a positive integer")
         return val
 
-    parser.add_argument('-w', '--word', action=WordAction, type=positive_int, nargs='?', help="Reverse W words at a time. (default W is 1, meaning all words are reversed)", default=False, metavar='W')
+    parser.add_argument('-w', '--word', action=WordAction, type=positive_int, nargs='?', help="Reverse W words at a time. Leaves whitespace intact. (default W is 1, meaning all words are reversed)", default=False, metavar='W')
     parser.add_argument('-c', '--character', action=CharacterAction,
             type=positive_int, nargs='?',
-            help="Reverse C characters at time. If --word is specified, reverse the characters in a word. (default C is 1, meaning all characters are reversed)",
+            help="Reverse C characters at time. If --word is specified, reverse the characters in a word, and leave the word order intact if W is 1. (default C is 1, meaning all characters are reversed)",
             default=True, metavar='C')
+    parse.add_argument('--ignorelines', action='store_true', default=False, help="Ignore newlines when reversing")
     parser.set_defaults(word_level=1, character_level=1)
     args = parser.parse_args()
+    if args.ignorelines:
+        text = args.input.read()
+        print(reverse(text, args.character, args.character_level, args.word, args.word_level))
+    else:
+        for line in args.input:
+            print(reverse(line, args.character, args.character_level, args.word, args.word_level))
