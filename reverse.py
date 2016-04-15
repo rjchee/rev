c3cadf909e0d677515f76d50fe3094aa5186dbd7
Raymond Chee <rjchee@gmail.com>
2016-04-14 23:45:22 -0500

    updated arguments

--- reverse.py
+++ reverse.py
@@ -1,7 +1,6 @@
 import argparse
 import sys
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Reverse the input")
     parser.add_argument('input', nargs='?', default=sys.stdin, type=argparse.FileType('r'), help="The input file (standard in by default)")
@@ -10,20 +9,27 @@ if __name__ == '__main__':
     class WordAction(argparse.Action):
         def __call__(self, parser, namespace, values, option_string=None):
             setattr(namespace, "word", True)
-            setattr(namespace, "word_level", values)
+            if values is not None:
+                setattr(namespace, "word_level", values)
             setattr(namespace, "character", False)
+
+    class CharacterAction(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            if values is not None:
+                setattr(namespace, "character_level", values)
+            setattr(namespace, "character", True)
 
     
     def positive_int(num):
         val = int(num)
-        if val <= 0:
+        if val <= 0 or float(num) != int(num):
             raise argparse.ArgumentTypeError("level must be a positive integer")
         return val
 
-    parser.add_argument('-w', '--word', action=WordAction, type=positive_int, nargs='?', help="Reverse L words at a time. (default 1)", default=False, metavar='L')
-    parser.add_argument('-c', '--character', action='store_true', help="Reverse characters. If word is specified, reverse the characters in a word", default=True)
-    parser.add_argument('-l', '--level', default=1, type=positive_int, help="the number of words or characters to reverse")
+    parser.add_argument('-w', '--word', action=WordAction, type=positive_int, nargs='?', help="Reverse W words at a time. (default W is 1, meaning all words are reversed)", default=False, metavar='W')
+    parser.add_argument('-c', '--character', action=CharacterAction,
+            type=positive_int, nargs='?',
+            help="Reverse C characters at time. If --word is specified, reverse the characters in a word. (default C is 1, meaning all characters are reversed)",
+            default=True, metavar='C')
+    parser.set_defaults(word_level=1, character_level=1)
     args = parser.parse_args()
-    print(args.input)
-    print(args.word)
-    print(args.character)
