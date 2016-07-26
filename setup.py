a8b3264c9f1e2678704eaebe1e26dfc6b4134680
Raymond Chee <rjchee@gmail.com>
2016-07-26 00:08:39 -0700

    uploaded to pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'rev',
+    packages = ['rev'],
+    version = '0.1.1',
+    description = 'Command line utility that reverses strings with varying granularities',
+    author = 'Raymond Chee',
+    author_email = 'rjchee@gmail.com',
+    url = 'https://github.com/rjchee/rev',
+    download_url = 'https://github.com/rjchee/rev/tarball/0.1',
+    keywords = ['reverse', 'strings', 'utility'],
+    classifiers = [],
+)
